70dacafcff2aa0b051b710381fae1fc99767e4e5
hla-ram2012 <hlaramirez@brc.pshs.edu.ph>
2026-09-08 13:07:30 +0800

    Refactor displayInfo method and update object1 grade
    Updated displayInfo method to remove Input_Name parameter and adjusted object1's grade level from 9 to 8. Cleaned up print statements for better readability.

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -14,26 +14,25 @@ class FriendinPisay:
     def updateSection(self, Input_Section):
         self.Section = Input_Section
     
-    def displayInfo(self, Input_Name):
+    def displayInfo(self):
         print(
 f"Name: {self.Name}\nGrade Level: {self.GradeLevel}\nSection: {self.Section}\nFriendship Level: {self.FriendshipLevel}" 
         )
-    return f"""{self.Name} has the following information:
-    Grade Level: {self.GradeLevel}
-    Section: {self.Section}
-    Friendship Level: {self.FriendshipLevel}"""
 
-object1 = FriendinPisay("Jeb Climaco", 9, "Silicon", "Close Friends")
+object1 = FriendinPisay("Jeb Climaco", 8, "Silicon", "Close Friends")
 object2 = FriendinPisay("Chance Caminar", 9, "Silicon", "Best Friends")
 
 print("---Before---")
 print(object1.displayInfo())
+print('')
 print(object2.displayInfo())
-
-print("Changing Grade Level of object1..")
+print('')
+print('')
+print(f"Changing Grade Level of {object1.Name}..")
 Input_GradeLevel = int(input("Grade Level of friend: "))
 object1.GradeLevel = Input_GradeLevel
-
+print('')
 print("---After---")
 print(object1.displayInfo())
+print('')
 print(object2.displayInfo())

d105a4b1e8613facd8bf2685211999382f2ac997
hla-ram2012 <hlaramirez@brc.pshs.edu.ph>
2026-09-16 21:20:31 +0800

    Update classRelationship.py

--- q1/classRelationship.py
+++ q1/classRelationship.py
@@ -12,15 +12,17 @@ class FriendGroup:
             print("List of members: ")
             for i in range(len(friendgroupList)):
                 print(friendgroupList[i])
-    def add(self, friendgroupList):
-        self.friendgroupList = friendgroupList.append(FriendinPisay)
-    def displayInfo(self, friendgroupName, friendgroupInterest, friendgroupDisinterest, friendgroupRelationship, friendgroupStatus, friendgroupList):
+    def add(self, friend):
+      list = self.friendgroupList
+      self.friendgroupList = list.append(friend)
+      
+    def displayInfo(self):
         print(f'''Friend Group Name: {self.friendgroupName}
         Common Interest: {self.friendgroupInterest}
         Common Disinterest: {self.friendgroupDisinterest}
         Relationship Structure: {self.__friendgroupRelationship}
         Status: {self.friendgroupStatus}
-        {displayList(self, friendgroupList)}''')
+        List of Members: {self.friendgroupList}''')
 
 class FriendinPisay:
     def __init__(self, Name, GradeLevel, Section, FriendshipLevel):
@@ -40,7 +42,7 @@ class FriendinPisay:
     
     def displayInfo(self):
         print(
-f"Name: {self.Name}\nGrade Level: {self.GradeLevel}\nSection: {self.Section}\nFriendship Level: {self.FriendshipLevel}" 
+f"Name: {self.Name}\nGrade Level: {self.GradeLevel}\nSection: {self.Section}\nFriendship Level: {self.__private_FriendshipLevel}" 
         )
 
 friend1 = FriendinPisay("Phil Bausa", 9, "Magnesium", "Close Friends")
@@ -65,6 +67,7 @@ fg2.add(friend1.Name)
 
 print("\n--- AFTER RELATIONSHIP ---")
 
-print("\nList of members in Friend Groups:")
-fg1.displayList()
-fg2.displayList()
+print("\nInformation of each Friend Groups:")
+fg1.displayInfo()
+print()
+fg2.displayInfo()
